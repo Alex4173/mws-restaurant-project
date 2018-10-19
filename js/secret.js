4a5612d74decf7335be77ac2f90b6180449d4086
Alex Jones <alexjones@Alexs-MacBook-Pro.local>
2018-10-18 23:25:35 -0400

    mapbox maps are working and API key is a secret

---
+++ js/secret.js
@@ -0,0 +1,6 @@
+class SECRET {
+  static get mapbox_key() {
+    //return your mapbox API mapbox_key
+    return "pk.eyJ1IjoiYWxleDQxNzMxNiIsImEiOiJjam5kYjd2MWIwcm5mM29ucjZpajV1cDd5In0.ZkYJtNzF6IrdglCXVp8hVw"
+  }
+}
